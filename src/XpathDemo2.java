fc7ebea3b6f9bbc74dad4b2e5d9a646a5bcf3588
Admin <amolujagare@gmail.com>
2026-03-10 22:41:34 +0530

    selenium

---
+++ src/XpathDemo2.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XpathDemo2 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demo.livehelperchat.com/site_admin/user/login/(r)/L3NpdGVfYWRtaW4vLw%3D%3D");
+
+            Thread.sleep(4000);
+        driver.findElement(By.xpath("//a[text()='Password reminder']")).click();
+
+    }
+    }
